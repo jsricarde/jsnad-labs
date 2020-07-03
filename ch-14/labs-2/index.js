0dde5c30712ee427f0b19c9990da40fe49185b82
jsricarde <jsricarde@gmail.com>
2020-07-03 17:57:28 -0500

    Add solution exercises to process chapter

--- ch-14/labs-2/index.js
+++ ch-14/labs-2/index.js
@@ -1,8 +1,8 @@
 'use strict'
-
+const os = require('os')
 setTimeout(() => {
-  console.log() // TODO output uptime of process
-  console.log() // TODO output uptime of OS
-  console.log() // TODO output total system memory
-  console.log() // TODO output total process memory
+  console.log(process.uptime()) // TODO output uptime of process
+  console.log(os.uptime()) // TODO output uptime of OS
+  console.log(os.totalmem()) // TODO output total system memory
+  console.log(process.memoryUsage().heapTotal) // TODO output total process memory
 }, 1000)
